e462d5574239dfa9c92b3026d64c1c3578d38ba5
ANKIT <itsankitagarwal000@gmail.com>
2022-12-30 00:28:06 +0530

    QUERY SELECTOR ALL

--- first git/main.js
+++ first git/main.js
@@ -1,2 +1,4 @@
 var header =document.querySelector("#main-header");
 header.getElementsByClassName.borderBottom="solid 4px #CCC"
+var items = document.querySelectorAll(".list-group-item:nth-child(1)")
+items.style.color="green"
